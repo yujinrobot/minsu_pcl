6c2d534bb531baa9cf3d6213fc09386edc40ee63
Kim Min Soo <ms.kim8717@gmail.com>
2012-08-14 21:36:11 +0900

    update

--- src/planar_segmentation.cpp
+++ src/planar_segmentation.cpp
@@ -1,70 +1,70 @@
-#include <iostream>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/io/pcd_io.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+// PCL specific includes
+#include <pcl/ros/conversions.h>
+#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/ModelCoefficients.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+
+ros::Publisher pub;
+
+void callback(const sensor_msgs::PointCloud2ConstPtr& cloud)
+{
+   //sensor_msgs::PointCloud2 cloud_filtered;
+   pcl::PointCloud<pcl::PointXYZ>::Ptr downsampled_XYZ(new pcl::PointCloud<pcl::PointXYZ>);
+   sensor_msgs::PointCloud2::Ptr cloud_filtered;
+   pcl::fromROSMsg (*cloud, *downsampled_XYZ);
+
+   pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+   pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+
+   // Create the segmentation object
+   pcl::SACSegmentation<pcl::PointXYZ> seg;
+   // Optional
+   seg.setOptimizeCoefficients (true);
+   // Mandatory
+   seg.setModelType (pcl::SACMODEL_PLANE);
+   seg.setMethodType (pcl::SAC_RANSAC);
+   seg.setMaxIterations (1000);
+   seg.setDistanceThreshold (0.01);
+   seg.setInputCloud (downsampled_XYZ);
+   seg.segment (*inliers, *coefficients);
+
+   if (inliers->indices.size () == 0)
+   {
+     PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+     return ;
+   }
+
+   std::cerr << "Model coefficients: "   << coefficients->values[0] << " "
+                                         << coefficients->values[1] << " "
+                                         << coefficients->values[2] << " "
+                                         << coefficients->values[3] << std::endl;
+
+   std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
+   pcl::toROSMsg (*downsampled_XYZ, *cloud_filtered);
+   pub.publish(cloud_filtered);
+}
+
+
+
 
 int
- main (int argc, char** argv)
+main (int argc, char** argv)
 {
-  pcl::PointCloud<pcl::PointXYZ> cloud;
+ // INITIALIZE ROS
+   ros::init (argc, argv, "planar_segmentation");
+   ros::NodeHandle nh;
+   ros::Subscriber sub = nh.subscribe("cloud", 1, callback);
+   pub = nh.advertise<sensor_msgs::PointCloud2> ("cloud_filtered", 1);
 
-  // Fill in the cloud data
-  cloud.width  = 15;
-  cloud.height = 1;
-  cloud.points.resize (cloud.width * cloud.height);
+   ros::spin();
 
-  // Generate the data
-  for (size_t i = 0; i < cloud.points.size (); ++i)
-  {
-    cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].z = 1.0;
-  }
-
-  // Set a few outliers
-  cloud.points[0].z = 2.0;
-  cloud.points[3].z = -2.0;
-  cloud.points[6].z = 4.0;
-
-  std::cerr << "Point cloud data: " << cloud.points.size () << " points" << std::endl;
-  for (size_t i = 0; i < cloud.points.size (); ++i)
-    std::cerr << "    " << cloud.points[i].x << " "
-                        << cloud.points[i].y << " "
-                        << cloud.points[i].z << std::endl;
-
-  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-  // Create the segmentation object
-  pcl::SACSegmentation<pcl::PointXYZ> seg;
-  // Optional
-  seg.setOptimizeCoefficients (true);
-  // Mandatory
-  seg.setModelType (pcl::SACMODEL_PLANE);
-  seg.setMethodType (pcl::SAC_RANSAC);
-  seg.setDistanceThreshold (0.01);
-
-  seg.setInputCloud (cloud.makeShared ());
-  seg.segment (*inliers, *coefficients);
-
-  if (inliers->indices.size () == 0)
-  {
-    PCL_ERROR ("Could not estimate a planar model for the given dataset.");
-    return (-1);
-  }
-
-  std::cerr << "Model coefficients: " << coefficients->values[0] << " "
-                                      << coefficients->values[1] << " "
-                                      << coefficients->values[2] << " "
-                                      << coefficients->values[3] << std::endl;
-
-  std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
-  for (size_t i = 0; i < inliers->indices.size (); ++i)
-    std::cerr << inliers->indices[i] << "    " << cloud.points[inliers->indices[i]].x << " "
-                                               << cloud.points[inliers->indices[i]].y << " "
-                                               << cloud.points[inliers->indices[i]].z << std::endl;
-
-  return (0);
+   return (0);
 }
