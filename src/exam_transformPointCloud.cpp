e5dfe9e34d9de711e5adf25fcfb50bfe31511288
Kim Min Soo <ms.kim8717@gmail.com>
2012-08-21 17:04:21 +0900

    Added launch file

--- src/exam_transformPointCloud.cpp
+++ src/exam_transformPointCloud.cpp
@@ -17,26 +17,28 @@ void cloudCb(const sensor_msgs::PointCloud2::ConstPtr& cloud)
 {
   std::cout << "cloud callback..." << std::endl;
   sensor_msgs::PointCloud2 cloud_out;
-/*
-  tf::TransformBroadcaster br;
-  tf::Transform transform;
-  transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-  transform.setRotation( tf::Quaternion(0, 1, 0) );
-  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "camera_depth_optical_frame", "test_frame"));
-*/
 
+
+//  tf::TransformBroadcaster br;
+//  tf::Transform transform;
+//  transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+//  transform.setRotation( tf::Quaternion(0, 1, 0) );a
+//  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "camera_depth_optical_frame", "test_frame"));
 
   tf::StampedTransform transform;
   static tf::TransformListener tf_listener;
 
-try {
-  tf_listener.lookupTransform("/test_frame", cloud->header.frame_id, cloud->header.stamp, transform);
-  pcl_ros::transformPointCloud("/test_frame", *cloud, cloud_out, tf_listener);
-  transform_pub.publish(cloud_out);
+  try {
 
-} catch (tf::TransformException ex){
-  ROS_ERROR("%s", ex.what());
-}
+    tf_listener.lookupTransform("/test_frame", cloud->header.frame_id, cloud->header.stamp, transform);
+    pcl_ros::transformPointCloud("/test_frame", *cloud, cloud_out, tf_listener);
+    transform_pub.publish(cloud_out);
+
+  } catch (tf::TransformException& ex){
+
+    ROS_ERROR("%s", ex.what());
+
+  }
 ///  cloud_out.header.frame_id = "/test_frame";
 
 }
