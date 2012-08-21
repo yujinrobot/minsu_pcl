5daf9d311d928767a4c628be386354855b76992f
Kim Min Soo <ms.kim8717@gmail.com>
2012-08-21 09:03:03 +0900

    update

---
+++ src/CMakeFiles/extract_sphere.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/extract_sphere.dir/extract_sphere.o"
+  "../bin/extract_sphere.pdb"
+  "../bin/extract_sphere"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/extract_sphere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/CMakeFiles/extract_sphere.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for extract_sphere.
+# This may be replaced when dependencies are built.
