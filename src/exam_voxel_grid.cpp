d7532ca539582f562502fa660c2859a5867f519e
Kim Min Soo <ms.kim8717@gmail.com>
2012-08-31 08:10:16 +0900

    deleted some files

--- src/exam_voxel_grid.cpp
+++ src/exam_voxel_grid.cpp
@@ -31,7 +31,7 @@ int
 main (int argc, char** argv)
 {
   // Initialize ROS
-  ros::init (argc, argv, "my_pcl_tutorial");
+  ros::init (argc, argv, "voxel_grid");
   ros::NodeHandle nh;
 
   // Create a ROS subscriber for the cloud point cloud
