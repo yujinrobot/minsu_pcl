03f682997862ec126008d90f855a557d0373863d
Kim Min Soo <ms.kim8717@gmail.com>
2012-08-31 08:35:37 +0900

    clean package

--- src/exam_planar_seg.cpp
+++ src/exam_planar_seg.cpp
@@ -1,62 +1,47 @@
-#include <iostream>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/io/pcd_io.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+// PCL specific includes
+#include <pcl/ros/conversions.h>
+#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/ModelCoefficients.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
 
-int
- main (int argc, char** argv)
+ros::Publisher pub;
+
+void callback(const sensor_msgs::PointCloud2ConstPtr& cloud)
 {
-  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transform_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-  // Fill in the cloud data
-  cloud.width  = 15;
-  cloud.height = 1;
-  cloud.points.resize (cloud.width * cloud.height);
+  sensor_msgs::PointCloud2::Ptr output_cloud(new sensor_msgs::PointCloud2);
 
-  // Generate the data
-  for (size_t i = 0; i < cloud.points.size (); ++i)
-  {
-    cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].z = 1.0;
-  }
+  // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
+  pcl::fromROSMsg (*cloud, *transform_cloud);
 
-  // Set a few outliers
-  cloud.points[0].z = 1.0;
-  cloud.points[1].z = 1.5;
-  cloud.points[3].z = -2.0;
-  cloud.points[6].z = 4.0;
-  cloud.points[9].z = 8.0;
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
 
-  std::cerr << "Point cloud data: " << cloud.points.size () << " points" << std::endl;
-  for (size_t i = 0; i < cloud.points.size (); ++i)
-    std::cerr << "    " << cloud.points[i].x << " "
-                        << cloud.points[i].y << " "
-                        << cloud.points[i].z << std::endl;
-
-  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-  // Create the segmentation object
+    // Create the segmentation object
   pcl::SACSegmentation<pcl::PointXYZ> seg;
   // Optional
   seg.setOptimizeCoefficients (true);
   // Mandatory
   seg.setModelType (pcl::SACMODEL_PLANE);
   seg.setMethodType (pcl::SAC_RANSAC);
+  //seg.setMaxIterations (1000);
   seg.setDistanceThreshold (0.01);
-
-  seg.setInputCloud (cloud.makeShared ());
+  seg.setInputCloud (transform_cloud);
   seg.segment (*inliers, *coefficients);
 
   if (inliers->indices.size () == 0)
   {
     PCL_ERROR ("Could not estimate a planar model for the given dataset.");
-    return (-1);
   }
-
   std::cerr << "Model coefficients: " << coefficients->values[0] << " "
                                       << coefficients->values[1] << " "
                                       << coefficients->values[2] << " "
@@ -64,9 +49,27 @@ int
 
   std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
   for (size_t i = 0; i < inliers->indices.size (); ++i)
-    std::cerr << inliers->indices[i] << "    " << cloud.points[inliers->indices[i]].x << " "
-                                               << cloud.points[inliers->indices[i]].y << " "
-                                               << cloud.points[inliers->indices[i]].z << std::endl;
+    std::cerr << inliers->indices[i] << "    " << transform_cloud->points[inliers->indices[i]].x << " "
+                                               << transform_cloud->points[inliers->indices[i]].y << " "
+                                               << transform_cloud->points[inliers->indices[i]].z << std::endl;
 
-  return (0);
+  // Convert the pcl/PointCloud to sensor_msgs/PointCloud2 data
+  pcl::toROSMsg (*transform_cloud, *output_cloud);
+  pub.publish(output_cloud);
+
+}
+
+
+int
+main (int argc, char** argv)
+{
+ // INITIALIZE ROS
+   ros::init (argc, argv, "planar_segmentation");
+   ros::NodeHandle nh;
+   ros::Subscriber sub = nh.subscribe("cloud", 1, callback);
+   pub = nh.advertise<sensor_msgs::PointCloud2> ("cloud_filtered", 1);
+
+   ros::spin();
+
+   return (0);
 }
